5d5f98bec539689f44357b599aee54d5d0242442
boyiling123 <200954810@qq.com>
2022-03-07 18:25:17 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "这是关于页面")
+		fmt.Fprint(w, "这是关于页面<a href='https://baidu.com'>访问百度</a>")
 	} else {
 		fmt.Fprint(w, "<h1>404</h1>")
 	}
